cbe9d195956796d287de503174472ba01cc7f33e
Joe Canero <caneroj1@tcnj.edu>
2013-12-30 13:23:51 -0500

    Update CreditCard.java

--- CS230-project1/CreditCard.java
+++ CS230-project1/CreditCard.java
@@ -1,4 +1,8 @@
 /**	Joe Canero, Project 1
+ * 	Class file for a Credit Card Object
+ * 	Each object will hold ccT, cvv, ccE, and cardNumber data fields.
+ * 	Each object also possesses a toString method that formats each credit card object's information
+ * 	and returns a neatly formatted string that has the information of the credit card
 */
 
 
@@ -8,11 +12,13 @@ import java.text.DecimalFormat;
 
 public class CreditCard 
 {
+	// private data for the credit card
 	private String ccT = "";
 	private int cvv;
 	private String ccE = "";
 	private long cardNumber;
 
+	// constructor method
 	public CreditCard (String ccT, long ccN, int cvv, String ccE)
 	{
 		this.ccT = ccT;
@@ -23,7 +29,9 @@ public class CreditCard
 		
 	}
 	
-	public String toString ()	//uses the different NumberFormat objects to format the cvv and cardNumber data correctly
+	// toString method
+	// uses the different NumberFormat objects to format the cvv and cardNumber data correctly
+	public String toString ()
 	{
 		NumberFormat fmt = new DecimalFormat ("000");
 		NumberFormat fmt2 = new DecimalFormat ("0000000000000000");
