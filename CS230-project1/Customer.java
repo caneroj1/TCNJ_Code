b27b0b0154f9fcd5a28b9c0d4a7daec2b68584ca
Joe Canero <caneroj1@tcnj.edu>
2013-12-30 13:31:10 -0500

    Update Customer.java

--- CS230-project1/Customer.java
+++ CS230-project1/Customer.java
@@ -1,13 +1,17 @@
 /**	Joe Canero, Project 1
+ * 	This is the class file for a Customer object. It inherits from Comparable and is therefore able to be compared to
+ * 	similar objects.
 */
 
 
 public class Customer implements Comparable
 {
+	// data for each Customer. Each customer has its personal information, mailing address, and phone number.
 	protected PersonalInformation personal;
 	protected MailingAddress mailing;
 	protected int number;
 
+	// constructor method
 	public Customer (int num, PersonalInformation info, MailingAddress mail)
 	{
 		personal = info;
@@ -15,6 +19,7 @@ public class Customer implements Comparable
 		number = num;
 	}
 	
+	// this toString method will neatly format the data in the Customer object and return a string
 	public String toString ()
 	{
 		String info = ( (personal.toString()) + "\t" + (mailing.toString()) + "\t");
@@ -39,24 +44,28 @@ public class Customer implements Comparable
 		return address;
 	}
 		
-	
-	public int compareTo (Object object1) //the Arrays.sort method will use this compareTo method to determine the order
+	// the Arrays.sort method will use this compareTo method to determine the order
+	// each Customer object will be compared to the other customer objects with this method's custom
+	// comparison routine that sorts each customer according to the following prioritization: Gender, ZipCode, Address
+	public int compareTo (Object object1) 
 	{
-		int returnV = 0;
-		int zip1 = this.getZipCode();
-		String gen1 = this.getGen();
-		String add1 = this.getAddress();
+		int returnV = 0;					// return value
+		int zip1 = this.getZipCode();				// zip code of calling customer object
+		String gen1 = this.getGen();				// gender of calling customer object		
+		String add1 = this.getAddress();			// address of calling customer object
 		
 		Customer custCompare = (Customer)object1;
-		int zip2 = custCompare.getZipCode();
-		String gen2 = custCompare.getGen();
-		String add2 = custCompare.getAddress();
+		int zip2 = custCompare.getZipCode();			// zip code of passed customer object
+		String gen2 = custCompare.getGen();			// gender of passed customer object
+		String add2 = custCompare.getAddress();			// address of passed customer object
 		
-		int genComp = gen1.compareTo(gen2);
-		int addComp = add1.compareTo(add2);
+		int genComp = gen1.compareTo(gen2);			// compare genders
+		int addComp = add1.compareTo(add2);			// compared addresses
 		
 		
-		
+		// the following logic will return the relative order of the two compared customers
+		// a return value of 0 indicates lexicographic equality, a number < 0 means the calling customer object 
+		// is lexicographically before, and a number > 0 means the calling customer object is lexicographically after
 		if(genComp > 0 && zip1 == zip2)
 			returnV = -1;
 		if(genComp < 0 && zip1 == zip2)
